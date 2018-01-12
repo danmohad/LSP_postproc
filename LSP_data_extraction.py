cb9bb4338cf07914aeb4949d53d9fdee148d62a1
Danyal Mohaddes <danmohad@gmail.com>
2018-01-12 14:14:38 -0800

    Adding radial velocity

--- LSP_data_extraction.py
+++ LSP_data_extraction.py
@@ -9,7 +9,7 @@ Danyal Mohaddes Khorassani
 """
 
 import sys
-import os
+#import os
 import tecplot as tp
 import numpy as np
 from os import walk
@@ -45,11 +45,12 @@ DP3_mat = np.zeros([len(x_bins),len(r_bins)])
 UP_X_mat = np.zeros([len(x_bins),len(r_bins)])
 UP_Y_mat = np.zeros([len(x_bins),len(r_bins)])
 UP_Z_mat = np.zeros([len(x_bins),len(r_bins)])
+UP_R_mat = np.zeros([len(x_bins),len(r_bins)])
 
 #get path to LSP files
 LSP_files_path = sys.argv[1]
-print 'Processing LSP files at path:'
-print LSP_files_path
+print ('Processing LSP files at path:')
+print (LSP_files_path)
 #get filenames
 #(_, _, LSP_filename_vec) = walk('C:\Users\Danyal\Box Sync\Stanford\Research\LES\Mastorakos_spray\LSP Data Extraction').next()
 (_, _, LSP_filename_vec) = walk(LSP_files_path).next()
@@ -59,7 +60,7 @@ if 'LSP_data_processed.npz' in LSP_filename_vec:
 #Loop over all files
 for LSP_filename in LSP_filename_vec:
     #print filename for progress tracking purposes
-    print 'Processing file:' + LSP_filename
+    print ('Processing file:') + LSP_filename
     
     #import data from lsp .plt file
     dataset = tp.data.load_tecplot(LSP_files_path + '/' + LSP_filename)
@@ -81,7 +82,7 @@ for LSP_filename in LSP_filename_vec:
     
     #Loop over all droplets in file
     #Assume NPAR_array is all ones (i.e. all parcels only have 1 droplet each)    
-    for i in xrange(NPAR_TOT):
+    for i in range(NPAR_TOT):
         #Determine if droplet is in a bin
         #if z_array[i] < dz and z_array[i] > -dz:
         #    if y_array[i] < y_bins.max() + dy and y_array[i] > y_bins.min() - dy:
@@ -89,8 +90,14 @@ for LSP_filename in LSP_filename_vec:
             if any(np.equal(x_array[i] < x_bins + dx,x_array[i] > x_bins - dx)):
                 idx_x = np.equal(x_array[i] < x_bins + dx,x_array[i] > x_bins - dx).argmax()
                 #idx_y = np.argmin(np.abs(y_bins - y_array[i]))
+                #calc droplet radial position
                 r_i = np.sqrt(np.square(y_array[i])+np.square(z_array[i]))
                 idx_r = np.argmin(np.abs(r_bins - r_i))
+                #calc droplet radial velocity
+                nr_vec_i = np.array([x_array[i],y_array[i],z_array[i]])/r_i
+                up_vec_i = np.array([UP_X_array[i],UP_Y_array[i],UP_Z_array[i]])
+                up_r_i = np.dot(up_vec_i,nr_vec_i)
+                
                 #Update droplet stats
                 NP_mat[idx_x,idx_r] += 1
                 TP_mat[idx_x,idx_r] += TP_array[i]
@@ -101,6 +108,7 @@ for LSP_filename in LSP_filename_vec:
                 UP_X_mat[idx_x,idx_r] += UP_X_array[i]
                 UP_Y_mat[idx_x,idx_r] += UP_Y_array[i]
                 UP_Z_mat[idx_x,idx_r] += UP_Z_array[i]
+                UP_R_mat[idx_x,idx_r] += up_r_i
     #delete dataset object
     del dataset
     
@@ -109,11 +117,11 @@ outfile_name = LSP_files_path + '/' + 'LSP_data_processed_azimuthal.npz'
 with open(outfile_name,'wb') as outfile:
     np.savez(outfile,NP_mat=NP_mat,TP_mat=TP_mat,MP_mat=MP_mat,DP_mat=DP_mat,\
              DP2_mat=DP2_mat,DP3_mat=DP3_mat,UP_X_mat=UP_X_mat,UP_Y_mat=UP_Y_mat,\
-             UP_Z_mat=UP_Z_mat,x_bins=x_bins,r_bins=r_bins)
+             UP_Z_mat=UP_Z_mat,UP_R_mat=UP_R_mat,x_bins=x_bins,r_bins=r_bins)
 
 #Print completion message
 #cwd = os.getcwd()
-print 'Processing complete.\nData saved at ' + outfile_name
+print ('Processing complete.\nData saved at ' + outfile_name)
 
     
 
